331a55ceb44cde8aab41074dccfb07d73015555b
Ilya <in.belsotra@gmail.com>
2022-05-30 14:51:39 +0300

    e

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(" Please write number(1,2,3 etc) ");
+                string str = Console.ReadLine();
+
+                try
+                {
+                    int n = Convert.ToInt32(str);
+                    Console.WriteLine($"\t" + "Your number is" + "\t" + str);
+
+                    for (int i = 1; i < 11; i++)
+                    {
+                        int m = i * n;
+                        Console.WriteLine("{0}*{1}={2}", i, n, m);
+
+                    }
+                    Console.WriteLine("\t" + "If you want to continue press button!");
+                    Console.ReadLine();
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please, enter valid  number ! ");
+                    Console.ReadLine();
+                }
+            }       
+        }
+    }
+}
--- Weekdays/Program.cs
+++ Weekdays/Program.cs
@@ -6,7 +6,63 @@ namespace Weekdays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Clear();
+                int DayNumber;
+
+                try
+                {
+                    Console.WriteLine("Please, enter the number (from 1 to 7)");
+                    DayNumber = int.Parse(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please, choose the correct form! Press 'Enter' and try again.");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                switch (DayNumber)
+
+                {
+                    case 1:
+                        Console.WriteLine("Monday"); // can write diferent form "1 - Monday" etc
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Tuesday");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Wednesday");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Thursday");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Friday");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Saturday");
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Sunday");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please, write correct form!");
+                        break;
+
+                }
+                Console.ReadLine();
+
+            }   
         }
     }
 }
